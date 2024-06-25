e24a712049768f27ec8cb098f8b1391903caf5be
Ricardo <ricardoserafim1920@gmail.com>
2024-06-24 21:36:59 -0300

    Domain created.

---
+++ DesafioPostosDeVacina.Domain/Entities/Entity.cs
@@ -0,0 +1,7 @@
+﻿namespace DesafioPostosDeVacina.Domain.Entities
+{
+    public abstract class Entity
+    {
+        public int Id { get; protected set; }
+    }
+}
---
+++ DesafioPostosDeVacina.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace DesafioPostosDeVacina.Domain.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> ObterTodosAsync();
+        Task<T> ObterPorIdAsync(int id);
+        Task AdicionarAsync(T entity);
+        Task AtualizarAsync(T entity);
+        Task ExcluirAsync(T entity);
+    }
+}
